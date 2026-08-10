9407a0e856cf53a9cc94ea276d77ce11e81120a8
Amey Mahadik <36587413+saapo-ka-baadshah@users.noreply.github.com>
2026-08-10 23:06:24 +0200

    Fixed the output validations

--- modules/kube/outputs.tf
+++ modules/kube/outputs.tf
@@ -1,11 +1,11 @@
 output "vm_id" {
-  value = proxmox_virtual_environment_vm.this.vm_id
+  value = proxmox_virtual_environment_vm.this[*].vm_id
 }
 
 output "name" {
-  value = proxmox_virtual_environment_vm.this.name
+  value = proxmox_virtual_environment_vm.this[*].name
 }
 
 output "node_name" {
-  value = proxmox_virtual_environment_vm.this.node_name
+  value = proxmox_virtual_environment_vm.this[*].node_name
 }
