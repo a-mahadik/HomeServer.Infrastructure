a10ded42b6d21c19db59c736a3f5739e204aa664
Amey Mahadik <36587413+saapo-ka-baadshah@users.noreply.github.com>
2026-08-11 23:14:41 +0200

    Added top level outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "ubuntu_vm_private_keys" {
+  value     = { for name, m in module.kube : name => m.ubuntu_vm_private_key }
+  sensitive = true
+}
+
+output "ubuntu_vm_passwords" {
+  value     = { for name, m in module.kube : name => m.ubuntu_vm_password }
+  sensitive = true
+}
